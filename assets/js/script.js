361b96ecc5786f0c9a61a3e853013f287eca22ef
Lucas da Silva Oliveira <107443575+LucasdaSilvaOliveira@users.noreply.github.com>
2025-07-29 18:27:06 -0300

    att

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,38 @@ $(".carousel-header-bottom").owlCarousel({
         }
     }
 });
+
+// Lógica das perguntas e respostas
+
+let btnsPerguntas = document.querySelectorAll(".btn-pergunta-item");
+// let ti
+let divRespostas = document.querySelectorAll(".div-resposta");
+let setas = document.querySelectorAll(".seta-down");
+
+btnsPerguntas.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+
+        let titleBtn = btn.firstElementChild;
+
+        if(titleBtn.style.fontWeight == "bold") {
+
+            titleBtn.style.fontWeight = "normal";
+        } else {
+
+            titleBtn.style.fontWeight = "bold";
+        }
+
+
+        divRespostas.forEach((divResp, indexResp) => {
+            if (index == indexResp) {
+                divResp.classList.toggle("toggle-class-resposta");
+            }
+        })
+
+        setas.forEach((seta, indexSeta) => {
+            if (index == indexSeta) {
+                seta.classList.toggle("toggle-class-seta");
+            }
+        })
+    })
+})
